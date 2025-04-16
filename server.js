6c9b9b95de75401e2f72bc00e5178db501e23ef0
Aya Abdel-Fattah <ayaaf@MacBookPro.fritz.box>
2025-04-16 21:55:11 +0200

    build in public

--- server.js
+++ server.js
@@ -80,3 +80,7 @@ app.listen(port, () => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
